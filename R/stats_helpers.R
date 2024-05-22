7038a41d3dafeefb57a8e525dd48bcf8e61c6aaf
Peter Stanbridge <peterstanbridge@peters-imac.home>
2024-05-22 22:03:39 +0100

    updates

---
+++ R/stats_helpers.R
@@ -0,0 +1,11 @@
+
+
+correlation_test <- function(data, sensemakerframework, columns = NULL, type = NULL, class = NULL,
+                             sig_ids = NULL, columns = NULL) {
+
+  if (!is.null(type)) {
+
+  }
+
+
+}
